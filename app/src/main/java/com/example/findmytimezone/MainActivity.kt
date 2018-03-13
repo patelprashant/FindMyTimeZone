a041170ea66cad044c2e33912968d927384d3120
Prashant <prashant.patel77@gmail.com>
2018-03-13 21:49:52 +1100

    Get seekbar into action

--- app/src/main/java/com/example/findmytimezone/MainActivity.kt
+++ app/src/main/java/com/example/findmytimezone/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.findmytimezone
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
+
+    private var seekBarView: SeekBar? = null
+    private var userTimeView: TextView? = null
+    private var localDate: Date = Date()
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userTimeView = this.userTime
+        seekBarView = this.seekBar
+        seekBarView!!.setOnSeekBarChangeListener(this)
+
     }
+
+    override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+        val progressText: String? = if (progress < 10) {
+            "0" + progress.toString()
+        } else {
+            progress.toString()
+        }
+        userTimeView?.text = progressText + ":00"
+
+        if (seekBar != null) localDate.hours= seekBar.progress
+        if(fromUser) {
+            localDate.minutes = 0
+        }
+        seekBar?.progress = localDate.hours
+    }
+
+    override fun onStartTrackingTouch(seekBar: SeekBar?) {
+    }
+
+    override fun onStopTrackingTouch(seekBar: SeekBar?) {
+    }
+
+
+
 }
